1686b20951419ef390f71d9634d0cf0d5796c033
Rico <emgaspar94@gmail.com>
2021-06-09 16:09:50 +0100

    sprint service layers complete

--- API_Testing_Mini_project/API_Testing_Mini_project/Service/DeleteFollowService.cs
+++ API_Testing_Mini_project/API_Testing_Mini_project/Service/DeleteFollowService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,31 @@ namespace API_Testing_Mini_project
 {
     class DeleteFollowService
     {
+        public ICallManager CallManager { get; set; }
+        public JObject JsonResponse { get; set; }
+
+        public string Type { get; set; }
+        public string Id { get; set; }
+        //public string IdSelected { get; set; }
+        //public DTO<> NameOfDTO { get; set; }
+        public string Response { get; set; }
+
+        public DeleteFollowService()
+        {
+            CallManager = new CallManager();
+            //NameOfDTO = new DTO<>();
+        }
+
+        public async Task MakeRequest(string type, string iD)
+        {
+            Type = type;
+            Id = iD;
+
+            Response = await CallManager.MakePutFollowRequestAsync(type, iD);
+
+            JsonResponse = JObject.Parse(Response);
+
+            //NameOfDTO.DeserealizeResponse(Response);
+        }
     }
 }
--- API_Testing_Mini_project/API_Testing_Mini_project/Service/HTTPManager/ICallManager.cs
+++ API_Testing_Mini_project/API_Testing_Mini_project/Service/HTTPManager/ICallManager.cs
@@ -9,6 +9,8 @@ namespace API_Testing_Mini_project
         Task<string> MakeGetArtistUserRequestAsync(string Id);
         Task<string> MakeGetFollowingArtistRequestAsync(string artist);
         Task<string> MakeGetAlbumRequestAsync(string name, string type);
+        Task<string> MakePutFollowRequestAsync(string type, string iD);
+        Task<string> MakeDeleteFollowRequestAsync(string type, string iD);
         Task<string> ExecuteRequestAsync(RestRequest request);
     }
 }
