4ee1963cf48455ce3d39dd6a78de262ef3493e09
UzairMK <uzairmkhan584@gmail.com>
2021-06-10 16:37:04 +0100

    test pics

--- API_Testing_Mini_project/API_Testing_Mini_project/Tests/GetAllPlaylistTests.cs
+++ API_Testing_Mini_project/API_Testing_Mini_project/Tests/GetAllPlaylistTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace API_Testing_Mini_project
 {
@@ -26,25 +21,25 @@ namespace API_Testing_Mini_project
             Assert.That(_service.CallManager.StatusCode, Is.EqualTo(200));
         }
 
-        [Category("Happy Path")]
-        [Test]
-        public void GivenGetPlaylistRequestMade_WhenResponseRecieved_ThenResponseNameShouldEqualToRicos_Room()
-        {
+        //[Category("Happy Path")]
+        //[Test]
+        //public void GivenGetPlaylistRequestMade_WhenResponseRecieved_ThenResponseNameShouldEqualToRicos_Room()
+        //{
 
-            var temp = _service.GetAllPlaylistDTO.Response.items.Where(x => x.name == "Rico's Room").ToList();
+        //    var temp = _service.GetAllPlaylistDTO.Response.items.Where(x => x.name == "Rico's Room").ToList();
             
-            Assert.That(temp[0].name, Is.EqualTo("Rico's Room"));
-        }
+        //    Assert.That(temp[0].name, Is.EqualTo("Rico's Room"));
+        //}
 
-        [Category("Happy Path")]
-        [Test]
-        public void GivenGetPlaylistRequestMade_WhenResponseRecieved_ThenResponseIdShouldEqualToId()
-        {
+        //[Category("Happy Path")]
+        //[Test]
+        //public void GivenGetPlaylistRequestMade_WhenResponseRecieved_ThenResponseIdShouldEqualToId()
+        //{
 
-            var temp = _service.GetAllPlaylistDTO.Response.items.Where(x => x.name == "Rico's Room").ToList();
+        //    var temp = _service.GetAllPlaylistDTO.Response.items.Where(x => x.name == "Rico's Room").ToList();
 
-            Assert.That(temp[0].id, Is.EqualTo("0hZjiDkbrvLpgbpVU7F0EM"));
-            Assert.That(temp[0].type, Is.EqualTo("playlist"));
-        }
+        //    Assert.That(temp[0].id, Is.EqualTo("0hZjiDkbrvLpgbpVU7F0EM"));
+        //    Assert.That(temp[0].type, Is.EqualTo("playlist"));
+        //}
     }
 }
