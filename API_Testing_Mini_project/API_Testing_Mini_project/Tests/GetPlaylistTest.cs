721848ae810d95049652af48e4522142deea9eb5
UzairMK <uzairmkhan584@gmail.com>
2021-06-10 15:40:03 +0100

    erics testes passed

---
+++ API_Testing_Mini_project/API_Testing_Mini_project/Tests/GetPlaylistTest.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace API_Testing_Mini_project
+{
+    class GetPlaylistTest_WithValidID
+    {
+        private GetPlaylistService _service;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _service = new GetPlaylistService();
+            _service.MakeRequest("7sf6IyQz843FD1Ndfmq3Wp");
+        }
+
+        [Category("Happy Path")]
+        [Test]
+        public void GivenGetPlaylistRequestMade_WhenResponseReceived_ThenResponseStatusShouldBe200()
+        {
+            Assert.That(_service.CallManager.StatusDescription, Is.EqualTo("OK"));
+        }
+
+        [Category("Happy Path")]
+        [Test]
+        public void GivenGetPlaylistRequestMade_WhenResponseRecieved_ThenResponseNameShouldEqualToMIH()
+        {
+            Assert.That(_service.GetPlaylistDTO.Response.name, Is.EqualTo("MIB"));
+        }
+
+        [Category("Happy Path")]
+        [Test]
+        public void GivenGetPlaylistRequestMade_WhenResponseRecieved_ThenResponseDescrShouldEqualToTest()
+        {
+            Assert.That(_service.GetPlaylistDTO.Response.description, Is.EqualTo("Test"));
+        }
+    }
+
+    class GetPlaylistTest_WithInvalidId
+    {
+        private GetPlaylistService _service;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _service = new GetPlaylistService();
+            _service.MakeRequest("invalidId");
+        }
+
+        [Category("Sad Path")]
+        [Test]
+        public void GivenGetPlaylistRequestMade_WhenResponseReceived_ThenResponseStatusShouldBe400()
+        {
+            Assert.That(_service.CallManager.StatusDescription, Is.EqualTo("Not Found"));
+        }
+
+        [Category("Sad Path")]
+        [Test]
+        public void GivenGetPlaylistRequestMade_WhenResponseRecieved_ThenResponseNameShouldEqualToMIH()
+        {
+            Assert.That(_service.JsonResponse["error"]["status"].ToString, Is.EqualTo("404"));
+        }
+
+        [Category("Sad Path")]
+        [Test]
+        public void GivenGetPlaylistRequestMade_WhenResponseRecieved_ThenResponseMessageShouldEqual()
+        {
+            Assert.That(_service.JsonResponse["error"]["message"].ToString, Is.EqualTo("Invalid playlist Id"));
+        }
+    }
+
+    class GetPlaylistTest_WithNoId
+    {
+        
+        private GetPlaylistService _service;
+
+        
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _service = new GetPlaylistService();
+            _service.MakeRequest("");
+        }
+
+        [Category("Sad Path")]
+        [Test]
+        public void GivenGetPlaylistRequestMade_WhenResponseReceived_ThenResponseStatusShouldNotFound()
+        {
+            Assert.That(_service.CallManager.StatusDescription, Is.EqualTo("Not Found"));
+        }
+    }
+}
